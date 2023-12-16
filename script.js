3c0a652a36fe713fd4f7aba88454046b3dba531e
Florin Miron <mironflorin123@yahoo.com>
2023-12-16 15:12:50 +0200

    create the functionality for fetching and displaying the recipes

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 import apiKey from "./api.js";
 
-const url = `https://api.spoonacular.com/recipes/complexSearch?query=risotto&apiKey=${apiKey}`;
+const recipeInput = document.getElementById('recipe');
+const recipesContainer = document.querySelector('.recipes-container');
+const searchButton = document.querySelector('.search-button');
+
+let userInput = '';
 
 async function fetchData() {
+    const url = `https://api.spoonacular.com/recipes/complexSearch?query=${userInput}&apiKey=${apiKey}&number=24`;
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
+    const results = data.results;
+    const recipesContent = results.map((recipe) => {
+        const {image: imageSrc, title} = recipe;
+        return `
+            <div class="recipe">
+                <div class="recipe-image">
+                    <img src="${imageSrc}" alt="${title}">
+                </div>
+                <h3 class="recipe-title">${title}</h3>
+            </div>
+        `
+    }).join('');
+    recipesContainer.innerHTML = recipesContent;
 }
+
+function searchRecipe(e) {
+    e.preventDefault();
+    userInput = recipeInput.value;
+    console.log(userInput);
+    fetchData();
+}
+
+searchButton.addEventListener('click', searchRecipe);
